0f2c5075d92247a15aed7cbbb361bf781cfe7a08
Robin A. Meade <robin.a.meade@gmail.com>
2025-03-18 11:50:42 -1000

    Add tooltip

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -9,10 +9,13 @@
 //   https://v0.dev/chat/theme-selector-component-F67qkxdQFPm?b=b_OX4zazdCUCa&p=0
 //   Note how the `ml-auto` class is used to push the Check icon to the right
 //   side of the dropdown menu item.
+//
+// * Tue Mar 18 2025 11:48:06 AM HST  Robin A. Meade <robin.a.meade@gmail.com>
+// - I added Tooltip from ShadCN
+// - I realized I should have used the Radio capabilities of ShadCN's DropDown menu.
+// - TODO: Used the Radio capabilities of ShadCN's DropDown menu
 
 'use client';
-
-import * as React from 'react';
 import { Moon, Sun, Check } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
@@ -23,31 +26,45 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 export default function ModeToggle() {
-  //const { setTheme } = useTheme();
   const { theme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light {theme === 'light' && <Check className="h-4 w-4 ml-auto" />}
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark {theme === 'dark' && <Check className="h-4 w-4 ml-auto" />}
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System {theme === 'system' && <Check className="h-4 w-4 ml-auto" />}
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <TooltipProvider>
+      <DropdownMenu>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Toggle theme</p>
+          </TooltipContent>
+        </Tooltip>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme('light')}>
+            Light {theme === 'light' && <Check className="h-4 w-4 ml-auto" />}
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme('dark')}>
+            Dark {theme === 'dark' && <Check className="h-4 w-4 ml-auto" />}
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme('system')}>
+            System {theme === 'system' && <Check className="h-4 w-4 ml-auto" />}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </TooltipProvider>
   );
 }
