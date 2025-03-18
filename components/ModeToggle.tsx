f60f9b861bdccafe1647007b18a7c64cb693c375
Robin A. Meade <robin.a.meade@gmail.com>
2025-03-17 20:13:31 -1000

    Add dark mode toggle

---
+++ components/ModeToggle.tsx
@@ -0,0 +1,53 @@
+// Note: This file is a modified version of the original ShadCN file
+// Original copied from https://ui.shadcn.com/docs/dark-mode/next
+//
+// Local modifications to the original ShadCN file
+// * Mon Mar 17 2025 07:55:35 PM HST  Robin A. Meade <robin.a.meade@gmail.com>
+// - Import Check from lucide-react
+// - Retrieve `theme` from useTheme so that we know the theme name
+// - Add a Check icon to the selected theme. Used logic and styling from
+//   https://v0.dev/chat/theme-selector-component-F67qkxdQFPm?b=b_OX4zazdCUCa&p=0
+//   Note how the `ml-auto` class is used to push the Check icon to the right
+//   side of the dropdown menu item.
+
+'use client';
+
+import * as React from 'react';
+import { Moon, Sun, Check } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export default function ModeToggle() {
+  //const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light {theme === 'light' && <Check className="h-4 w-4 ml-auto" />}
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark {theme === 'dark' && <Check className="h-4 w-4 ml-auto" />}
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System {theme === 'system' && <Check className="h-4 w-4 ml-auto" />}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
