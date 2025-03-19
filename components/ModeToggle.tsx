d0027b3f13ae0731ac1446d3e3fe6219fe886e7f
Robin A. Meade <robin.a.meade@gmail.com>
2025-03-18 15:50:33 -1000

    Remove non-working CSS transitions
    The CSS transitions were not working.
    
    Maybe the parent element had a change at the same time and perhaps
    this causes the child animation to cancel.
    
    Anyway, I removed them.
    
    The result is easier to understand.

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -14,6 +14,7 @@
 
 import * as React from 'react';
 import { Moon, Sun } from 'lucide-react';
+
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
@@ -68,7 +69,6 @@ export default function ModeToggle() {
       }, 0);
     }
   };
-
   return (
     <TooltipProvider>
       <DropdownMenu open={open} onOpenChange={handleOpenChange}>
@@ -76,8 +76,8 @@ export default function ModeToggle() {
           <TooltipTrigger asChild>
             <DropdownMenuTrigger asChild>
               <Button variant="outline" size="icon">
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <Sun className="h-[1.2rem] w-[1.2rem] dark:hidden" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] hidden dark:block" />
                 <span className="sr-only">Toggle theme</span>
               </Button>
             </DropdownMenuTrigger>
