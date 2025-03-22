1e2c3a5ab511121374ec5859396b18da6ed2f3cd
Robin A. Meade <robin.a.meade@gmail.com>
2025-03-21 21:04:07 -1000

    sync

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import { Moon, Sun } from 'lucide-react';
 
 function CssTransitionDark() {
   return (
-    <div className="relative">
-      <Sun className="h-96 w-96 rotate-0 scale-100 transition-transform duration-[3s] dark:rotate-180 dark:scale-0" />
-      <Moon className="absolute left-0 top-0 h-96 w-96 rotate-360 scale-0 transition-transform duration-[3s] dark:rotate-0 dark:scale-100" />
-    </div>
+    <>
+      <h1 className="text-9xl">Demo</h1>
+      <div className="relative">
+        <Sun className="h-96 w-96 rotate-0 scale-100 transition-transform duration-[3s] dark:rotate-180 dark:scale-0" />
+        <Moon className="absolute left-0 top-0 h-96 w-96 rotate-360 scale-0 transition-transform duration-[3s] dark:rotate-0 dark:scale-100" />
+      </div>
+    </>
   );
 }
 
