ea6d159f4c23b844de379e6fe0ba927ed0fe37be
Robin A. Meade <robin.a.meade@gmail.com>
2025-03-21 19:44:53 -1000

    sync

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased  min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  min-h-screen transition-colors duration-[3s]`}
       >
         {/* Parent container for your entire app */}
 
