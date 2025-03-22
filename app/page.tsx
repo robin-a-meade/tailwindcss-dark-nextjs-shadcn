3e66b3595facb27ed049713c4d8070bf16725a94
Robin A. Meade <robin.a.meade@gmail.com>
2025-03-21 21:22:19 -1000

    Got it!

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,19 @@ import { Moon, Sun } from 'lucide-react';
 function CssTransitionDark() {
   return (
     <>
-      <h1 className="text-9xl transition-colors duration-[3s]">Demo</h1>
+      <h1 className="text-9xl">Demo</h1>
       <div className="relative">
         <Sun className="h-96 w-96 rotate-0 scale-100 transition-transform duration-[3s] dark:rotate-180 dark:scale-0" />
         <Moon className="absolute left-0 top-0 h-96 w-96 rotate-360 scale-0 transition-transform duration-[3s] dark:rotate-0 dark:scale-100" />
       </div>
       <p>
-        Use of <b>next-themes</b> causes the transition to be instaneous.
+        <s>
+          Use of <b>next-themes</b> causes the transitions to be instaneous.
+        </s>
+      </p>
+      <p>
+        Update: Nah, I had `disableTransitionOnChange` set on the
+        ThemeProvider!`
       </p>
     </>
   );
